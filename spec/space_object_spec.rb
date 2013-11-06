8a00f6ddecb11a8a932f8fb58198fd647b11faa9
Robert Fruchtman <rfruchtose@gmail.com>
2013-11-05 19:02:17 -0800

    Add test for SpaceObject::parse.

--- spec/space_object_spec.rb
+++ spec/space_object_spec.rb
@@ -4,4 +4,14 @@ describe SpaceObject do
   it "should have a VERSION constant" do
     expect(subject.const_get('VERSION')).not_to be_empty
   end
+
+  describe "::parse" do
+    let(:test_document) { 'foo bar' }
+
+    it "creates a new SpaceObject::Parser" do
+      instance = SpaceObject::Parser.new(test_document)
+      SpaceObject::Parser.stub(:new).and_return(instance)
+      SpaceObject.parse(test_document) and expect(SpaceObject::Parser).to have_received(:new).with(test_document)
+    end
+  end
 end
